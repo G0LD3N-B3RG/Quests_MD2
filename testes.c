1e73a669e2ce2914446b9683edc0063019a067a4
Gabriel <gabrielgoldenmoita@gmail.com>
2025-10-17 15:43:52 -0300

    commit 7

--- testes.c
+++
@@ -1,5 +0,0 @@
-printf("===============================\n");
-        printf("           FATORES:\n");
-        printf("     N1 = %ld = %ld x %ld\n", N1, p, N1/p);
-        printf("     N2 = %ld = %ld x %ld\n", N2, q, N2/q);
-        printf("===============================\n");
